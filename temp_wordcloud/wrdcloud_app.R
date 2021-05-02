3d331ea1f05a0d5b654f8f21fba3d2370c13a7ef
vivienneprince <vivie.prince@gmail.com>
2021-05-01 21:02:35 -0400

    wordcloud updates finalized

--- temp_wordcloud/wrdcloud_app.R
+++ temp_wordcloud/wrdcloud_app.R
@@ -1,46 +1,68 @@
-library(tidyverse)
 library(shiny)
-library(plotly)
+library(wordcloud2)
+library(tidyverse)
 
+datapath <- '/Users/viv/Downloads/DATA'
 
-df <- read_csv(file.path('dataverse_files/debate_streaming.csv')) 
+# MAIN
+ts.df <- read_csv(file.path('ts_df.csv'))  # <------------------ data import (ts.df)
+channel.options <- unique(ts.df$channel)
+channel.options.pretty <- c("ABC","Fox","NBC")
+wordcloud_backgroundcolor <- "#F0F3F5"  # <------------------------- color options for sara
+wordcloud_textpallete <- c("#181D27","#272F3F","#08090D","#2D334E")  # <------------------------- color options for sara
 
-
-# Define UI for application that draws a histogram
+# UI
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("usr.zoom",
+                  "Word Cloud Zoom in/ out: ",
+                  min = 1,
+                  max = 10,
+                  value = 1),
+      selectInput("usr.channel", 
+                  "Channel:",
+                  choices = channel.options.pretty,
+                  selected = "ABC")
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      wordcloud2Output("wcloud"),
+      HTML("<br><br><br>"),
+      dataTableOutput('table')
     )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+# SERVER
+server <- function(input, output, session) {
+  output$wcloud <- renderWordcloud2({
+    usr.channel.no <- which(channel.options.pretty == input$usr.channel)
+    temp.tab <- ts.df %>% 
+      filter(channel == channel.options[usr.channel.no]) %>% 
+      select(correction) %>% count(correction, name = "freq") %>%
+      arrange(desc(freq))
+    
+    temp.tab %>% wordcloud2(fontFamily = "didot", 
+                            size = input$usr.zoom,  # <--------------- adjustable parameter "zoom" [1, 10]
+                            minSize = 3,
+                            rotateRatio = .15,
+                            gridSize = 7.5,
+                            ellipticity = 1,
+                            backgroundColor = wordcloud_backgroundcolor,
+                            color = wordcloud_textpallete)
+  })
+  
+  output$table <- renderDataTable({
+    temp.tab <- ts.df %>% 
+      filter(channel == channel.options[usr.channel.no]) %>%
+      select(correction) %>%
+      count(correction, name = "Frequency") %>%
+      arrange(desc(Frequency)) %>%
+      rename(c("correction" = "Word")) %>%
+      mutate(Proportion = paste0(round (Frequency / sum(Frequency) * 100, digits = 1), "%"))
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
